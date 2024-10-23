5d4d7153ef24da924a642912146b9857ae792717
7SepRash <jasleen_k@ee.iitr.ac.in>
2024-10-23 10:15:56 +0530

    added-dev

--- client.go
+++ client.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	// Connect to the chat server
-	conn, err := net.Dial("tcp", "localhost:8080")
+	conn, err := net.Dial("tcp", "10.81.17.131:8080")
 	if err != nil {
 		fmt.Println("Error connecting to server:", err)
 		os.Exit(1)
@@ -20,18 +20,12 @@ func main() {
 
 	// Goroutine to handle receiving messages from the server
 	go func() {
-		scanner := bufio.NewScanner(conn)
-		for scanner.Scan() {
-			msg := scanner.Text()
-			fmt.Println(msg)
-		}
+		
 	}()
 
 	// Send messages to the server
 	input := bufio.NewReader(os.Stdin)
 	for {
-		fmt.Print("> ")
-		message, _ := input.ReadString('\n')
-		fmt.Fprint(conn, message)
+		
 	}
 }
--- server.go
+++ server.go
@@ -18,7 +18,7 @@ func main() {
 	var err error
 
 	// Start listening for incoming TCP connections
-	ln, err := net.Listen("tcp", ":8080")
+	ln, err := net.Listen("tcp", "10.81.17.131:8080")
 	if err != nil {
 		fmt.Println("Error starting the server:", err)
 		os.Exit(1)
@@ -42,9 +42,6 @@ func main() {
 func handleClient(conn net.Conn) {
 	defer conn.Close()
 
-	clientMux.Lock()
-	clients[conn] = conn.RemoteAddr().String() // Add the client to the list
-	clientMux.Unlock()
 
 	fmt.Println("New client connected:", conn.RemoteAddr())
 
@@ -52,21 +49,9 @@ func handleClient(conn net.Conn) {
 	broadcastMessage(fmt.Sprintf("%s joined the chat", conn.RemoteAddr().String()), conn)
 
 	// Read incoming messages from the client
-	scanner := bufio.NewScanner(conn)
-	for scanner.Scan() {
-		msg := scanner.Text()
-		if strings.TrimSpace(msg) == "" {
-			continue
-		}
-
-		// Broadcast the message to all clients
-		broadcastMessage(fmt.Sprintf("%s: %s", clients[conn], msg), conn)
-	}
+	
 
 	// Handle client disconnection
-	clientMux.Lock()
-	delete(clients, conn)
-	clientMux.Unlock()
 
 	fmt.Println("Client disconnected:", conn.RemoteAddr())
 	broadcastMessage(fmt.Sprintf("%s left the chat", conn.RemoteAddr().String()), conn)
@@ -75,13 +60,6 @@ func handleClient(conn net.Conn) {
 
 // Broadcasts a message to all clients except the sender
 func broadcastMessage(message string, sender net.Conn) {
-	fmt.Println("Broadcasting:", message)
-	clientMux.Lock()
-	for client := range clients {
-		if client != sender {
-			fmt.Fprintln(client, message)
-		}
-	}
-	clientMux.Unlock()
+	
 }
 
